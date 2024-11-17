808659d879534995abaf8ee78d03ce3eaf09008b
Jayden-Boomer <89496979+Jayden-Boomer@users.noreply.github.com>
2024-11-17 01:39:59 -0600

    a

--- server/app.js
+++ server/app.js
@@ -1,18 +1,18 @@
 //app.js
 
 import { createServer } from 'http'
+var fs = require('fs');
+
 const port = 8080
 
 // Create a server object:
 const server = createServer(function (req, res) {
-
-    // Write a response to the client
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    // res.end('Hello World!');
-
-    // End the response 
-    res.end('Hello World!');
-})
+    fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+})//.listen(8080); //the server object listens on port 8080
 
 // Set up our server so it will listen on the port
 server.listen(port, function (error) {
